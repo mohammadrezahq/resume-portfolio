1249d05fdcdb05be47cabc516de0c60699078944
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-02-23 15:31:17 +0330

    Update Skills

--- src/components/views/Skills/Skill.vue
+++ src/components/views/Skills/Skill.vue
@@ -1,16 +1,10 @@
 <template>
-  <div class="flex items-center mr-2 bg-white px-2 py-1 rounded-xl shadow-md">
+  <div class="flex items-center mr-2 bg-white px-2 py-1 rounded-xl shadow-md cursor-pointer">
     <p class="text-md">{{name}}</p>
-    <Circle :percent="percent" />
   </div>
 </template>
 <script>
-import Circle from './Circle.vue'
-
 export default {
-  props: ["percent", "name"],
-  components: {
-    Circle
-  }
+  props: ["name"],
 };
 </script>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -5,51 +5,60 @@
       <div class="my-2">
         <b class="block my-1">{{ $t("skills.pl") }}</b>
         <div class="flex">
-          <Skill percent="100" name="PHP" />
-          <Skill percent="75" name="JavaScript" title="I know it's not :)" />
+          <Skill name="PHP" />
+          <Skill name="JavaScript" title="I know it's not :)" />
+          <!-- <Skill name="Python" />
+          <Skill name="Dart" title="New to this" /> -->
+        </div>
+      </div>      
+      <div class="my-2">
+        <b class="block my-1">Backend</b>
+        <div class="flex">
+          <Skill name="Laravel" />
+          <Skill name="Lumen" />
+          <!-- <Skill name="Django" /> -->
         </div>
       </div>
       <div class="my-2">
-        <b class="block my-1">{{ $t("skills.f") }}</b>
+        <b class="block my-1">Frontend</b>
         <div class="flex">
-          <Skill percent="100" name="Laravel" />
-          <Skill percent="50" name="Lumen" />
-          <Skill percent="75" name="Vue Js" />
-          <Skill percent="100" name="Tailwind" />
-          <Skill percent="100" name="Bootstrap" />
-          <Skill percent="100" name="Jquery" />
+          <Skill name="Vue Js" />
+          <!-- <Skill name="Flutter" /> -->
+          <Skill name="Tailwind" />
+          <Skill name="Bootstrap" />
+          <Skill name="Jquery" />
+          <Skill name="HTML" />
+          <Skill name="CSS" />
+          <Skill name="Scss" />
         </div>
       </div>
       <div class="my-2">
         <b class="block my-1">{{ $t("skills.d") }}</b>
         <div class="flex">
-          <Skill percent="100" name="MySQL" />
-          <Skill percent="100" name="MongoDB" />
-        </div>
-      </div>
-      <div class="my-2">
-        <b class="block my-1">{{ $t("skills.wt") }}</b>
-        <div class="flex">
-          <Skill percent="100" name="HTML" />
-          <Skill percent="100" name="CSS" />
-          <Skill percent="75" name="Scss" />
+          <Skill name="MySQL" />
+          <Skill name="MongoDB" />
+          <Skill name="SQlite" />
+          <Skill name="Redis" />
         </div>
       </div>
       <div class="my-2">
         <b class="block my-1">{{ $t("skills.t") }}</b>
         <div class="flex">
-          <Skill percent="50" name="Git" />
-          <Skill percent="75" name="Composer" />
-          <Skill percent="50" name="Npm" />
+          <Skill name="Electron js" />
+          <Skill name="Git" />
+          <Skill name="Composer" />
+          <Skill name="Npm" />
+          <!-- <Skill name="Docker" /> -->
         </div>
       </div>
       <div class="my-2">
         <b class="block my-1">{{ $t("skills.o") }}</b>
         <div class="flex">
-          <Skill percent="50" name="Linux" />
-          <Skill percent="75" name="Lamp" />
-          <Skill percent="100" name="Wordpress" />
-          <Skill percent="100" name="Wordpress Theme Development" />
+          <Skill name="Linux" />
+          <Skill name="Lamp" />
+          <Skill name="Wordpress" />
+          <Skill name="Wordpress Theme Development" />
+          <Skill name="Figma" />
         </div>
       </div>
     </div>
