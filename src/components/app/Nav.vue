e7aef42504bca7b9616ec7bb0c4f934a86873549
mohammadrezahq <mohammadrezah.official@gmail.com>
2022-03-08 02:37:31 +0330

    Sticky Nav

--- src/components/app/Nav.vue
+++ src/components/app/Nav.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="nav text-left m-5">
-    <ToBottom>
-      <div v-if="show">
-        <img src="@/assets/avatar.jpg" class="rounded-lg w-56 mb-2" />
-        <h1 :class="($i18n.locale == 'en') ? 'text-xl font-normal' : 'text-xl font-normal text-right'">{{ $t("nav.name") }}</h1>
-        <p :class="($i18n.locale == 'en') ? 'font-light mb-2' : 'font-light mb-2 text-sm text-right'">Web Developer | PHP/Laravel</p>
-        <hr />
-      </div>
-    </ToBottom>
-    <ToLeft>
-      <div v-if="show">
-        <Navlink to="/@/about">{{ $t("nav.about") }}</Navlink>
-        <Navlink to="/@/skills">{{ $t("nav.skills") }}</Navlink>
-        <Navlink to="/@/projects">{{ $t("nav.projects") }}</Navlink>
-        <Navlink to="/@/education">{{ $t("nav.education") }}</Navlink>
-        <Navlink to="/@/employment-history">{{ $t("nav.employment") }}</Navlink>
-      </div>
-    </ToLeft>
+    <div class="sticky top-2">
+      <ToBottom>
+        <div v-if="show">
+          <img src="@/assets/avatar.jpg" class="rounded-lg w-56 mb-2" />
+          <h1 :class="($i18n.locale == 'en') ? 'text-xl font-normal' : 'text-xl font-normal text-right'">{{ $t("nav.name") }}</h1>
+          <p :class="($i18n.locale == 'en') ? 'font-light mb-2' : 'font-light mb-2 text-sm text-right'">Web Developer | PHP/Laravel</p>
+          <hr />
+        </div>
+      </ToBottom>
+      <ToLeft>
+        <div v-if="show">
+          <Navlink to="/@/about">{{ $t("nav.about") }}</Navlink>
+          <Navlink to="/@/skills">{{ $t("nav.skills") }}</Navlink>
+          <Navlink to="/@/projects">{{ $t("nav.projects") }}</Navlink>
+          <Navlink to="/@/education">{{ $t("nav.education") }}</Navlink>
+          <Navlink to="/@/employment-history">{{ $t("nav.employment") }}</Navlink>
+        </div>
+      </ToLeft>
+    </div>
   </div>
 </template>
 
